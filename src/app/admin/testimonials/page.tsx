6a397fc9545fbc3d506159612aa070bb686268d0
WebWizard <thewebwizard00@gmail.com>
2025-11-10 15:54:06 +0000

    ReferenceError: AlertDialogTrigger is not defined     at https://9000-fi

--- src/app/admin/testimonials/page.tsx
+++ src/app/admin/testimonials/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 import Image from "next/image"
 import { MoreHorizontal, PlusCircle, Trash2, SquarePen } from "lucide-react"
@@ -21,6 +20,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import {
   DropdownMenu,
